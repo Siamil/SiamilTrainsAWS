c5dc051860f7e2980a1f37adc2823ed8b04e1cd6
Unknown <smoogus@gmail.com>
2019-02-22 22:53:42 +0100

    add tests to the project

---
+++ AWSLambdaApi.Tests/TeamsControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AWSLambdaApi.Controllers;
+using AWSLambdaApi.Models;
+using Xunit;
+
+namespace AWSLambdaApi.Tests
+{
+    public class TeamsControllerTest
+    {
+         TeamsController controller = new TeamsController();
+
+        [Fact]
+        public void QueryTeamListReturnsCorrectTeams()
+        {
+            List<Team> teams = new List<Team>(controller.GetAllTeams());
+            Assert.Equal(teams.Count, 2);
+
+        }
+    }
+}
---
+++ AWSLambdaApi/Controllers/TeamsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AWSLambdaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSLambdaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        public TeamsController()
+        {
+        }
+        [HttpGet]
+        public IEnumerable<Team> GetAllTeams()
+        {
+            return new Team[] { new Team("one"), new Team("two") };
+
+        }
+
+    }
+}
